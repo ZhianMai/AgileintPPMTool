3909d771451fb05e85cde8cbdf145e3b557c54c6
Zhian Mai <zmai@ucsd.edu>
2021-08-11 10:54:41 -0700

    Add project task frontend

---
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const addProjectTask =
+  (backlog_id, project_task, history) => async (dispatch) => {
+    const res = await axios.post(`/api/backlog/${backlog_id}`, project_task);
+    history.push(`/projectBoard/${backlog_id}`);
+  };
