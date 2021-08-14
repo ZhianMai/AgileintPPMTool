a2279f6c9487f4f14e06f7a68eeaff03aded9a8a
Zhian Mai <zmai@ucsd.edu>
2021-08-14 04:29:31 -0700

    Security setup 1

---
+++ PPMToolFullStack/src/main/java/io/johnston/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package io.johnston.ppmtool.repositories;
+
+import io.johnston.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ PPMToolFullStack/src/main/java/io/johnston/ppmtool/services/UserService.java
@@ -0,0 +1,12 @@
+package io.johnston.ppmtool.services;
+
+import io.johnston.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+}
