4ce6afeb477d755dbc2f820066fd3f0e0ddce372
Zhian Mai <zmai@ucsd.edu>
2021-08-26 16:49:13 -0700

    Login logic: fix refreshing logout, session expired redirect

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import setJWTToken from "../securityUtils/setJWTToken";
 import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
 import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
@@ -21,30 +21,34 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
 
 export const login = (LoginRequest) => async (dispatch) => {
   try {
-    // Post => Login Request
+    // post => Login Request
     const res = await axios.post("/api/users/login", LoginRequest);
-
-    // Extract token from res.data
+    // extract token from res.data
     const { token } = res.data;
-
-    // Store the token in the localStorage
+    // store the token in the localStorage
     localStorage.setItem("jwtToken", token);
-
-    // Set token in header *** Important ***
+    // set our token in header ***
     setJWTToken(token);
-
-    // Decode token on React
+    // decode token on React
     const decoded = jwt_decode(token);
-
-    // Dispatch to security reducer
+    // dispatch to our securityReducer
     dispatch({
       type: SET_CURRENT_USER,
       payload: decoded,
     });
-  } catch (error) {
+  } catch (err) {
     dispatch({
       type: GET_ERRORS,
-      payload: error.response.data,
+      payload: err.response.data,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("jwtToken");
+  setJWTToken(false);
+  dispatch({
+    type: SET_CURRENT_USER,
+    payload: {},
+  });
 };
