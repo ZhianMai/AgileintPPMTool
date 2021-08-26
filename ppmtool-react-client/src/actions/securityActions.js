2e2c7ff1217c9b9bd9c55606a948b889c05c24a0
Zhian Mai <zmai@ucsd.edu>
2021-08-26 01:16:38 -0700

    Login JWT

--- ppmtool-react-client/src/actions/securityActions.js
+++ ppmtool-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
@@ -13,6 +15,36 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
     dispatch({
       type: GET_ERRORS,
       payload: err.response.data,
+    });
+  }
+};
+
+export const login = (LoginRequest) => async (dispatch) => {
+  try {
+    // Post => Login Request
+    const res = await axios.post("/api/users/login", LoginRequest);
+
+    // Extract token from res.data
+    const { token } = res.data;
+
+    // Store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+
+    // Set token in header *** Important ***
+    setJWTToken(token);
+
+    // Decode token on React
+    const decoded = jwt_decode(token);
+
+    // Dispatch to security reducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
     });
   }
 };
